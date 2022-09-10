5e553d79bfa6040094f2d53ac3bf94243ef4eae1
Borislav Stoyanov <bostoy@protonmail.com>
2022-09-10 12:53:05 +0300

    🚀 Deploy NftMarketplace script

---
+++ deploy/01-deploy-nftMarketplace.js
@@ -0,0 +1,22 @@
+const { network, ethers } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    const nftMarketplace = await deploy("NftMarketplace", {
+        from: deployer,
+        args: [],
+        logs: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        await verify(nftMarketplace.address, [])
+    }
+}
+
+module.exports.tags = ["all", "nftmarketplace"]
