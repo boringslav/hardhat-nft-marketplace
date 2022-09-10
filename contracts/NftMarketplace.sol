fd4900b99a2461aa2af4c2ed7c2b77a0c2d7ec89
Borislav Stoyanov <bostoy@protonmail.com>
2022-09-10 11:51:08 +0300

    ✨ Withdraw function

--- contracts/NftMarketplace.sol
+++ contracts/NftMarketplace.sol
@@ -10,6 +10,8 @@ error NftMarketplace__AlreadyListed(address nftAddress, uint256 tokenId);
 error NftMarketplace__NotOwner();
 error NftMarketplace__NotListed(address nftAddress, uint256 tokenId);
 error NftMarketplace__PriceNotMet(address nftAddress, uint256 tokenId, uint256 price);
+error NftMarketPlace__NoProceeds();
+error NftMarketPlace__TransferFailed();
 
 contract NftMatketplace is ReentrancyGuard {
     struct Listing {
@@ -142,5 +144,19 @@ contract NftMatketplace is ReentrancyGuard {
     ) external isListed(nftAddress, tokenId) isOwner(nftAddress, tokenId, msg.sender) {
         s_listings[nftAddress][tokenId].price = newPrice;
         emit ItemListed(msg.sender, nftAddress, tokenId, newPrice);
+    }
+
+    function withdrawProceeds() external {
+        uint256 proceeds = s_proceeds[msg.sender];
+
+        if (proceeds <= 0) {
+            revert NftMarketPlace__NoProceeds();
+        }
+        s_proceeds[msg.sender] = 0;
+        (bool success, ) = payable(msg.sender).call{value: proceeds}("");
+
+        if (!success) {
+            revert NftMarketPlace__TransferFailed();
+        }
     }
 }
