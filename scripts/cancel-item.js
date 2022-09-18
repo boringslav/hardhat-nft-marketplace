16534dc69c137664568adf3b0a264094cc531463
Borislav Stoyanov <bostoy@protonmail.com>
2022-09-18 20:30:16 +0300

    ✨ CancelItem script

---
+++ scripts/cancel-item.js
@@ -0,0 +1,21 @@
+const { ethers, network } = require("hardhat")
+const { moveBlocks } = require("../utils/move-blocks")
+
+const TOKEN_ID = 2 //Check moralis api to see which Listing tokenId you want to delete
+;(async () => {
+    const nftMarketplace = await ethers.getContract("NftMarketplace")
+    const basicNft = await ethers.getContract("BasicNft")
+
+    const tx = await nftMarketplace.cancelListing(basicNft.address, TOKEN_ID)
+    await tx.wait(1)
+    console.log("NFT cancelled")
+
+    if (network.config.chainId == "31337") {
+        await moveBlocks(2, 1000)
+    }
+})()
+    .then(() => process.exit(0))
+    .catch((e) => {
+        console.error(e)
+        process.exit(1)
+    })
